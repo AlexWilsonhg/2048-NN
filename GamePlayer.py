644dfa948ab9c9b3cb5a5dbbbc1c0506a1d3266b
Alex <awilson.hg@gmail.com>
2019-05-28 11:21:33 -0400

    Added move delays to gameplayer

--- GamePlayer.py
+++ GamePlayer.py
@@ -4,7 +4,7 @@ from Mover import Mover
 
 class GamePlayer:
 
-    def __init__(self, brain, numGames):
+    def __init__(self, brain, numGames, moveDelay):
         self.driver = webdriver.Chrome('C:\\Users\\wat\\Desktop\\chromedriver')
         if(self.driver):
             self.driver.get('https://play2048.co/')
@@ -13,21 +13,30 @@ class GamePlayer:
         self.boardReader = BoardReader(self.driver)
         self.mover = Mover(self.driver)
         self.scores = []
+        self.moveDelay = moveDelay
+        self.timeSinceLastMove = 0
 
 
-    def TakeTurn(self):
-        if(self.boardReader.GameOver()):
-            ## click the new game button
-            ## reset all components, store final score
-            self.scores.append(self.boardReader.GetScore())
-            self.boardReader.NewGame()
-            self.boardReader.Reset()
-            self.mover.Reset()
-            self.numGames -= 1
-            
-            
+    def Update(self, deltaTime):
+        self.timeSinceLastMove += deltaTime
+        if(self.timeSinceLastMove < self.moveDelay):
+            return
         else:
-            tiles = self.boardReader.GetTiles()
-            moves = self.brain.GetMoves(tiles)
-            self.mover.DoMove(moves, self.boardReader)
+            self.timeSinceLastMove = 0.0
+            if(self.boardReader.GameOver()):
+                ## click the new game button
+                ## reset all components, store final score
+                self.scores.append(self.boardReader.GetScore())
+                self.boardReader.NewGame()
+                self.boardReader.Reset()
+                self.mover.Reset()
+                self.numGames -= 1
+
+            if(self.numGames == 0):
+                return
+                
+            else:
+                tiles = self.boardReader.GetTiles()
+                moves = self.brain.GetMoves(tiles)
+                self.mover.DoMove(moves, self.boardReader)
             
--- Main.py
+++ Main.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
 from GamePlayer import GamePlayer
 from Brains.RandomBrain import RandomBrain
+import time
 
-players = []
+activePlayers = []
+inactivePlayers = []
 
 
-players.append(GamePlayer(RandomBrain(), 2))
+activePlayers.append(GamePlayer(RandomBrain(), 2, 1))
+activePlayers.append(GamePlayer(RandomBrain(), 5, 1))
+
+def Step(deltaTime):
+    for i in activePlayers:
+        i.Update(deltaTime)
+        
+   
+def Run():
+    deltaTime = time.time()
+    while(len(activePlayers) > 0):
+        start = time.time()
+        Step(deltaTime)
+        end = time.time()
+        deltaTime = end-start
 
